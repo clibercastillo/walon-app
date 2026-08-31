b661f0550f6e1d3de6feea1c7a17eaf07036af83
clibercastillo <clibercastillo@outlook.com>
2026-08-30 23:17:23 -0500

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  authUrl: 'https://TU_DOMINIO_O_IP/api/auth',
-  stadiumsUrl: 'https://TU_DOMINIO_O_IP/api/stadiums',
-  bookingsUrl: 'https://TU_DOMINIO_O_IP/api/bookings',
-  notificationsUrl: 'https://TU_DOMINIO_O_IP/api/notifications',
+  authUrl: 'http://3.131.90.240/api/auth',
+  stadiumsUrl: 'http://3.131.90.240/api/stadiums',
+  bookingsUrl: 'http://3.131.90.240/api/bookings',
+  notificationsUrl: 'http://3.131.90.240/api/notifications',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  authUrl: 'http://localhost:8081/api/auth',
-  stadiumsUrl: 'http://localhost:8082/api/stadiums',
-  bookingsUrl: 'http://localhost:8083/api/bookings',
-  notificationsUrl: 'http://localhost:8084/api/notifications',
+  authUrl: 'http://3.131.90.240/api/auth',
+  stadiumsUrl: 'http://3.131.90.240/api/stadiums',
+  bookingsUrl: 'http://3.131.90.240/api/bookings',
+  notificationsUrl: 'http://3.131.90.240/api/notifications',
 };
